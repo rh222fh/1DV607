aa9b868f6553e217cf1439c3f23aae7d17bd8acf
rh222fh <rh222fh@student.lnu.se>
2017-09-22 17:17:19 +0200

    Boat class

--- src/Back/Boat.java
+++ src/Back/Boat.java
@@ -1,4 +1,21 @@
 package Back;
 
 public class Boat {
+    private Type bt;
+    private int length;
+
+    public enum Type { Sailboat, Motorsailer, Canoe, Other }
+
+    public Boat(Type bt, int length) {
+        this.bt = bt;
+        this.length = length;
+    }
+
+    public Boat() {}
+
+    public Type getType() { return bt; }
+    public void setType(Type bt) { this.bt = bt; }
+
+    public int getLength() { return length; }
+    public void setLength(int length) { this.length = length; }
 }
