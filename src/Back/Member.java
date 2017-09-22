01f0c0a63f91afaf2df64665985765eb9534fece
sk222sq <sk222sq@student.lnu.se>
2017-09-22 17:43:55 +0200

    Updated Member class.

--- src/Back/Member.java
+++ src/Back/Member.java
@@ -1,4 +1,44 @@
 package Back;
 
+import java.util.ArrayList;
+
 public class Member {
+
+    private String pNumber;
+    private String name;
+    private ArrayList<Boat> boats = new ArrayList<>();
+
+    public Member(String number, String mName){
+        pNumber = number;
+        name = mName;
+
+    }
+
+    public void setName(String s){
+        name = s;
+    }
+
+    public void setPersonalNumber(String s){
+        pNumber = s;
+    }
+
+    public void addBoat(Boat b){
+        boats.add(b);
+    }
+
+    public ArrayList<Boat> getBoats(){
+        return boats;
+    }
+
+    public int countBoats(){
+        return boats.size();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public String getpNumber(){
+        return pNumber;
+    }
 }
