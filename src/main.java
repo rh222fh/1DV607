df15377aa1243a1b0931e5d171a8cba7ee11446e
rh222fh <rh222fh@student.lnu.se>
2017-09-22 18:26:11 +0200

    Tests n stuff

--- src/main.java
+++ src/main.java
@@ -1,10 +1,26 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
+import Back.Boat;
+import Back.Member;
+import Back.Registry;
 
 public class main {
 
     public static void main(String[] args) {
 
-        System.out.print("hej");
+        Registry reg = new Registry();
+        Member mem =  new Member("890705-2990", "Rilind");
+        Member mem2 =  new Member("890705-2990", "Alexander");
+        Member mem3 =  new Member("890705-2990", "Sandra");
+
+        //mem.addBoat(boat);
+        reg.addMember(mem);
+        reg.addMember(mem2);
+        reg.addMember(mem3);
+
+        Boat boat = new Boat(Boat.Type.Canoe, 10);
+        reg.getMember(1).addBoat(boat);
+
+        mem.addBoat(boat);
+        reg.getCompactList();
 
 
     }
