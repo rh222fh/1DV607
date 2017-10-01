a1999b42d0360c38814f4a69d8ecd0533c53f1c5
rh222fh <rh222fh@student.lnu.se>
2017-10-01 11:49:32 +0200

    Javadoc

--- src/model/Boat.java
+++ src/model/Boat.java
@@ -1,25 +1,55 @@
 package model;
 
+/**
+ * Class that represents a boat object
+ */
 public class Boat {
 
-    //Fields//
+    /* Fields */
     private Type bt;
     private int length;
 
+    /**
+     * Enum with all the different boat types
+     */
     public enum Type { sailboat, motorsailer, canoe, other }
 
-    //Constructor//
+    /**
+     * Constructor for boat with parameters
+     * @param bt Specifies the type of the boat
+     * @param length Specifies the length of the boat
+     */
     public Boat(Type bt, int length) {
         this.bt = bt;
         this.length = length;
     }
 
+    /**
+     * Empty constructor for boat
+     */
     public Boat() {}
 
-    //Methods//
+    /**
+     * Method for getting the type
+     * @return Returns the boat type
+     */
     public Type getType() { return bt; }
+
+    /**
+     * Method for setting the boat type
+     * @param bt Specifies the type
+     */
     public void setType(Type bt) { this.bt = bt; }
 
+    /**
+     * Method for getting the length
+     * @return Returns the boat length
+     */
     public int getLength() { return length; }
+
+    /**
+     * Method for setting the length
+     * @param length Specifies the length
+     */
     public void setLength(int length) { this.length = length; }
 }
